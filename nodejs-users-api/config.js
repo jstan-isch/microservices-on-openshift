11b019bea3c8ca302cca233aaa54287a34bee426
Dinesh <janepelladinesh97@gmail.com>
2019-08-16 17:07:42 +0530

    Update config.js

--- nodejs-users-api/config.js
+++ nodejs-users-api/config.js
@@ -3,9 +3,9 @@ var port = 8081;
 var mongoHost = process.env[mongoServiceName + "_SERVICE_HOST"];
 var mongoPort = process.env[mongoServiceName + "_SERVICE_PORT"];
 //console.log("mongohost:port ="+mongoHost+":"+mongoPort);
-module.exports = {
-	'secret': 'saregama',
-	//'database':'mongodb://localhost/demo'
-	'database': 'mongodb://'+process.env.MONGODB_USER+':'+process.env.MONGODB_PASSWORD+'@'+process.env.MONGODB_SERVICE_HOST+':27017/'+process.env.MONGODB_DATABASE
-	//'database': 'mongodb://'+process.env.MONGODB_USER+':'+process.env.MONGODB_PASSWORD+'@'+mongoHost+':'+mongoPort+'/'+process.env.MONGODB_DATABASE
-};
+mongoose.connection.openUri('mongodb://'+process.env.MONGODB_USER+':'+process.env.MONGODB_PASSWORD+'@'+process.env.MONGODB_SERVICE_HOST+':27017/'+process.env.MONGODB_DATABASE)
+  .once('open', () => console.log('Good to go !'))
+  .on('error', (error) => {
+    console.warn('Warning', error);
+  });
+
