08bb9aea9646da134292ffb86f4d8abac863d6a2
Dinesh <janepelladinesh97@gmail.com>
2019-08-19 13:30:14 +0530

    Update sample.py

--- python-email-api/sample.py
+++ python-email-api/sample.py
@@ -27,6 +27,16 @@ try:
         cursor.execute("select database();")
         record = cursor.fetchone()
         print("Your connected to database: ", record)
+    mySql_Create_Table_Query = """CREATE TABLE Laptop ( 
+                             Id int(11) NOT NULL,
+                             Name varchar(250) NOT NULL,
+                             Price float NOT NULL,
+                             Purchase_date Date NOT NULL,
+                             PRIMARY KEY (Id)) """
+    cursor = connection.cursor()
+    result = cursor.execute(mySql_Create_Table_Query)
+    print("Laptop Table created successfully ")
+
 except Error as e:
     print("Error while connecting to MySQL", e)
 finally:
